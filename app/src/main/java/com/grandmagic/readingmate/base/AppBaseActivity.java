0a4d0cc0b4574030fbfcbad63e7059dcfb555f4c
zhangmengqi <542018543@qq.com>
2017-01-23 14:38:13 +0800

    1.最低版本更改 2.加入butterknife

--- app/src/main/java/com/grandmagic/readingmate/base/AppBaseActivity.java
+++ app/src/main/java/com/grandmagic/readingmate/base/AppBaseActivity.java
@@ -11,7 +11,6 @@ import android.support.v7.app.AppCompatActivity;
 public class AppBaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
-
-
+        super.onCreate(savedInstanceState);
     }
 }
