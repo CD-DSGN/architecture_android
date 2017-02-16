c988929f2b82d1a89cc84f9df542507da06a691b
lpslpsl <1245441817@qq.com>
2017-02-16 10:53:12 +0800

    首页布局

--- app/src/main/java/com/grandmagic/readingmate/fragment/HomeFragment.java
+++ app/src/main/java/com/grandmagic/readingmate/fragment/HomeFragment.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -15,6 +16,7 @@ import android.widget.ImageView;
 import android.widget.PopupWindow;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.grandmagic.readingmate.R;
 import com.grandmagic.readingmate.activity.CaptureActivity;
 import com.grandmagic.readingmate.activity.MainActivity;
@@ -51,7 +53,6 @@ public class HomeFragment extends AppBaseFragment {
     @BindView(R.id.iv_camera)
     ImageView mIvCamera;
     private View rootview;
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -88,21 +89,31 @@ public class HomeFragment extends AppBaseFragment {
         for (int i = 0; i < 120; i++) {
             mStrings.add(i + "helloworld");
         }
-        mRecyclerview.setAdapter(new CommonAdapter<String>(mContext, R.layout.item_simpletext, mStrings) {
+        mRecyclerview.setItemAnimator(new DefaultItemAnimator());
+        mRecyclerview.setAdapter(new CommonAdapter<String>(mContext, R.layout.item_homepage, mStrings) {
+            @Override
+            public void onViewHolderCreated(ViewHolder holder, View itemView) {
+              AutoUtils.auto(itemView);
+            }
 
             @Override
             protected void convert(ViewHolder holder, String mS, int position) {
-
+                Glide.with(mContext).load("https://ss1.baidu.com/6ONXsjip0QIZ8tyhnq/it/u=1937046046,1905495319&fm=58")
+                        .into((ImageView) holder.getView(R.id.iv_conver));
+                Glide.with(mContext).load("https://ss2.baidu.com/6ONYsjip0QIZ8tyhnq/it/u=1460995985,2991423940&fm=58")
+                        .into((ImageView) holder.getView(R.id.avatar));
             }
         });
     }
 
-    @OnClick({R.id.pop_scan, R.id.pop_search, R.id.tv_title})
+    @OnClick({R.id.pop_scan, R.id.pop_search, R.id.tv_title,R.id.iv_camera,R.id.iv_search})
     public void onClick(View view) {
         switch (view.getId()) {
+            case R.id.iv_camera:
             case R.id.pop_scan:
                 startActivity(new Intent(getActivity(), CaptureActivity.class));
                 break;
+            case R.id.iv_search:
             case R.id.pop_search:
                 startActivity(new Intent(getActivity(), SearchActivity.class));
                 break;
@@ -113,6 +124,8 @@ public class HomeFragment extends AppBaseFragment {
                     showEmptyView();
                 }
                 break;
+
+
         }
     }
 }
---
+++ app/src/main/java/com/grandmagic/readingmate/view/RVItemAnimation.java
@@ -0,0 +1,10 @@
+package com.grandmagic.readingmate.view;
+
+import android.support.v7.widget.DefaultItemAnimator;
+
+/**
+ * Created by lps on 2017/2/16.
+ */
+
+public class RVItemAnimation   extends DefaultItemAnimator{
+}
