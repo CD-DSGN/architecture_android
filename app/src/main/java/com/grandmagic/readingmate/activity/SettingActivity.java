4b64f1543a5ab388e124260d5944e362f4bda6ac
zhangmengqi <542018543@qq.com>
2017-02-17 12:38:06 +0800

    1.增加文本编辑型对话框的基本组件 2.完善反馈功能

--- app/src/main/java/com/grandmagic/readingmate/activity/SettingActivity.java
+++ app/src/main/java/com/grandmagic/readingmate/activity/SettingActivity.java
@@ -3,7 +3,10 @@ package com.grandmagic.readingmate.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.SwitchCompat;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.TextView;
 
 import com.grandmagic.readingmate.R;
@@ -12,6 +15,7 @@ import com.grandmagic.readingmate.base.AppBaseResponseCallBack;
 import com.grandmagic.readingmate.bean.request.FeedBackRequestBean;
 import com.grandmagic.readingmate.model.FeedBackModel;
 import com.grandmagic.readingmate.model.LoginModel;
+import com.grandmagic.readingmate.ui.CustomDialog;
 import com.grandmagic.readingmate.utils.KitUtils;
 import com.grandmagic.readingmate.utils.SPUtils;
 import com.grandmagic.readingmate.utils.ViewUtils;
@@ -22,15 +26,25 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class SettingActivity extends AppBaseActivity {
+public class SettingActivity extends AppBaseActivity implements CompoundButton.OnCheckedChangeListener {
     @BindView(R.id.feed_back)
     TextView mFeedBack;
     @BindView(R.id.logout)
     TextView mLogout;
     @BindView(R.id.tv_setting_current_version)
     TextView mTvSettingCurrentVersion;
-    @BindView(R.id.switch_setting_push)
+
     SwitchCompat mSwitchSettingPush;
+    @BindView(R.id.test)
+    Button mTest;
+    @BindView(R.id.tv_setting_account)
+    TextView mTvSettingAccount;
+    @BindView(R.id.tv_setting_mobile)
+    TextView mTvSettingMobile;
+    @BindView(R.id.tv_seeting_points)
+    TextView mTvSeetingPoints;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,12 +56,15 @@ public class SettingActivity extends AppBaseActivity {
 
     //完成一些网络数据的获取及相应view的更新
     private void loadData() {
-
-
+        //1.获取绑定手机、账号、积分
+        mTvSettingAccount.setText("多情剑客无情剑");
+        mTvSettingMobile.setText("13518143569");
+        mTvSeetingPoints.setText("2000");
     }
 
     private void initView() {
         //设置版本信息
+        mSwitchSettingPush = (SwitchCompat) findViewById(R.id.switch_setting_push);
         String Version_name = KitUtils.getVersionName(SettingActivity.this);
         mTvSettingCurrentVersion.setText(Version_name);
         //根据sp信息，设置推送通知的打开和关闭状态
@@ -64,7 +81,7 @@ public class SettingActivity extends AppBaseActivity {
                     @Override
                     public void onSuccee(NovateResponse<Object> response) {
                         ViewUtils.showToast(SettingActivity.this,
-                                "退出成功");
+                                getString(R.string.logout_success));
                         jumpToLoginActivity();
                     }
 
@@ -85,7 +102,13 @@ public class SettingActivity extends AppBaseActivity {
 
     //处理用户反馈
     private void feedback() {
-        new FeedBackModel(SettingActivity.this, new FeedBackRequestBean("缺少某功能"),
+        //显示dialog
+        showFeedBackDialog();
+
+    }
+
+    private void sendfeedBackNetworkData(String content) {
+        new FeedBackModel(SettingActivity.this, new FeedBackRequestBean(content),
                 new AppBaseResponseCallBack<NovateResponse<Object>>(SettingActivity.this, true) {
                     @Override
                     public void onSuccee(NovateResponse<Object> response) {
@@ -94,7 +117,32 @@ public class SettingActivity extends AppBaseActivity {
                 }).feedBack();
     }
 
-    @OnClick({R.id.feed_back, R.id.logout, R.id.switch_setting_push})
+    private void showFeedBackDialog() {
+        final CustomDialog customDialog = new CustomDialog(this);
+        customDialog.setTitle(getString(R.string.your_feedback));
+        customDialog.setMaxNum(300).setOnBtnOnclickListener(new CustomDialog.BtnOnclickListener() {
+            @Override
+            public void onYesClick() {
+                String content = customDialog.getMessage();
+                if (TextUtils.isEmpty(content)) {
+                    ViewUtils.showToast(SettingActivity.this, getString(R.string.feedback_content_empty));
+                    return;
+                }else{
+                    sendfeedBackNetworkData(content);
+                }
+                customDialog.dismiss();
+            }
+
+            @Override
+            public void onNoClick() {
+                customDialog.dismiss();
+            }
+        });
+        customDialog.show();
+    }
+
+
+    @OnClick({R.id.feed_back, R.id.logout, R.id.test})
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.feed_back:
@@ -103,13 +151,22 @@ public class SettingActivity extends AppBaseActivity {
             case R.id.logout:
                 logout();
                 break;
-            case R.id.switch_setting_push:
-                //保存开关状态
-                SPUtils.getInstance().setPushSetting(SettingActivity.this, mSwitchSettingPush.isChecked());
+            case R.id.test:
+                boolean checked = SPUtils.getInstance().getPushSetting(SettingActivity.this);
+                ViewUtils.showToast(SettingActivity.this, "checked:" + checked);
+                break;
             default:
                 break;
         }
     }
 
 
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        if (buttonView.getId() == R.id.switch_setting_push) {
+            //        保存开关状态
+            ViewUtils.showToast(SettingActivity.this, "开关状态" + isChecked);
+            SPUtils.getInstance().setPushSetting(SettingActivity.this, isChecked);
+        }
+    }
 }
---
+++ app/src/main/java/com/grandmagic/readingmate/ui/CustomDialog.java
@@ -0,0 +1,191 @@
+package com.grandmagic.readingmate.ui;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.grandmagic.readingmate.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+/**
+ * TODO: document your custom view class.
+ */
+public class CustomDialog extends Dialog {
+    @BindView(R.id.title)
+    TextView titleTv;
+    @BindView(R.id.message)
+    EditText messageEt;
+    @BindView(R.id.no)
+    Button no;
+    @BindView(R.id.yes)
+    Button yes;
+    @BindView(R.id.clear_text)
+    ImageView mClearText;
+    @BindView(R.id.num_hint)
+    TextView mNumHint;
+
+    private String titleStr;//从外界设置的title文本
+    private String messageStr;//从外界设置的消息文本
+
+    private BtnOnclickListener mBtnOnclickListener;
+    private String yesStr;
+    private String noStr;
+
+    private int max_num;
+
+    /**
+     * 设置取消按钮的显示内容和监听
+     */
+    public void setOnBtnOnclickListener(BtnOnclickListener btnOnclickListener) {
+        this.mBtnOnclickListener = btnOnclickListener;
+    }
+
+
+    public CustomDialog(Context context) {
+        super(context, R.style.CustomDialog);
+        yesStr = context.getString(R.string.confirm);
+        noStr = context.getString(R.string.cancel);
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.custome_dialog_edittext_layout);
+        ButterKnife.bind(this);
+        //按空白处不能取消动画
+        setCanceledOnTouchOutside(false);
+        //初始化界面数据
+        initData();
+        initView();
+    }
+
+    private void initView() {
+        //跟踪字数变化
+        mNumHint.setText("0/" + max_num);
+        messageEt.addTextChangedListener(new TextWatcher() {
+            private CharSequence temp;
+            private int selectionStart;
+            private int selectionEnd;
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                temp = s;
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                int number = max_num - s.length();
+                mNumHint.setText("" + number + "/" + max_num);
+                selectionStart = messageEt.getSelectionStart();
+                selectionEnd = messageEt.getSelectionEnd();
+                if (temp.length() > max_num) {
+                    s.delete(selectionStart - 1, selectionEnd);
+                    int tempSelection = selectionEnd;
+                    messageEt.setText(s);
+                    messageEt.setSelection(tempSelection);//设置光标在最后
+                }
+            }
+        });
+    }
+
+
+    //设置最大字数限制
+    public CustomDialog setMaxNum(int num) {
+        max_num = num;
+        return this;
+    }
+
+
+    /**
+     * 初始化界面控件的显示数据
+     */
+    private void initData() {
+        //如果用户自定了title和message
+        if (titleStr != null) {
+            titleTv.setText(titleStr);
+        }
+        if (messageStr != null) {
+            messageEt.setText(messageStr);
+        }
+        //如果设置按钮的文字
+        if (yesStr != null) {
+            yes.setText(yesStr);
+        }
+        if (noStr != null) {
+            no.setText(noStr);
+        }
+    }
+
+
+    /**
+     * 从外界Activity为Dialog设置标题
+     *
+     * @param title
+     */
+    public void setTitle(String title) {
+        titleStr = title;
+    }
+
+    /**
+     * 从外界Activity为Dialog设置dialog的message
+     *
+     * @param message
+     */
+    public void setMessage(String message) {
+        messageStr = message;
+        if (!TextUtils.isEmpty(messageStr)) {
+            messageEt.setText(messageStr);
+        }
+    }
+
+    /**
+     * 获取dialog文本框的内容
+     */
+    public String getMessage() {
+        return messageEt.getText().toString();
+    }
+
+    @OnClick({R.id.no, R.id.yes, R.id.clear_text})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.no:
+                mBtnOnclickListener.onNoClick();
+                break;
+            case R.id.yes:
+                mBtnOnclickListener.onYesClick();
+                break;
+            case R.id.clear_text:
+                messageEt.setText("");
+                break;
+            default:
+                break;
+        }
+    }
+
+
+    /**
+     * 设置确定按钮和取消被点击的接口
+     */
+    public interface BtnOnclickListener {
+        public void onYesClick();
+        public void onNoClick();
+    }
+
+}
