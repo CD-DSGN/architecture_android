ff09ebee0f0881dea2f4d0d7e7064399493a4a21
zhangmengqi <542018543@qq.com>
2017-03-21 10:30:02 +0800

    对401做特殊处理

--- novate/src/main/java/com/tamic/novate/exception/NovateException.java
+++ novate/src/main/java/com/tamic/novate/exception/NovateException.java
@@ -18,7 +18,7 @@ import retrofit2.adapter.rxjava.HttpException;
  */
 public class NovateException {
 
-    private static final int UNAUTHORIZED = 401;
+    public static final int UNAUTHORIZED = 401;
     private static final int FORBIDDEN = 403;
     private static final int NOT_FOUND = 404;
     private static final int REQUEST_TIMEOUT = 408;
