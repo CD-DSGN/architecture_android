3882e66ed27d092ad4136d823c98b11feb739609
zhangmengqi <542018543@qq.com>
2017-03-23 16:34:48 +0800

    1.分页工具类 2.个人页面增加刷新和加载更多

---
+++ app/src/main/java/com/grandmagic/readingmate/utils/Page.java
@@ -0,0 +1,46 @@
+package com.grandmagic.readingmate.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by zhangmengqi on 2017/3/23.
+ */
+
+public class Page {
+    public List list;
+    public int page_size;
+    public int cur_page = 1;
+    public int total_num = -1;
+
+    public Page(List list, int size) {
+        this.list = list;
+        this.page_size = size;
+    }
+
+    public void refresh(List al) {
+        list.clear();
+        cur_page = 1;
+        list.addAll(al);
+    }
+
+    public void more(List al) {
+        if (al == null) {
+            return;
+        }
+
+        if (list.size()/total_num == 0) {
+            list.addAll(al);
+        }else{
+            int last_num = list.size()%total_num;
+            List sub = (ArrayList) al.subList(last_num + 1, al.size());
+            list.addAll(sub);
+        }
+        cur_page = list.size() / page_size + 1;
+    }
+
+    public boolean hasMore() {
+        return !(list.size() == total_num);
+    }
+
+}
