4847e4bf862cb6fc7dc710e539c1e7a207c2b948
lpslpsl <1245441817@qq.com>
2017-04-13 18:44:16 +0800

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit b53360f66034c67e02e9545f80ce5cd40415a8eb, reversing
    changes made to ba3890b6f9aab13b72c8f901ba16f8a8e56938dd.

--- app/src/main/java/com/grandmagic/readingmate/model/BookModel.java
+++ app/src/main/java/com/grandmagic/readingmate/model/BookModel.java
@@ -52,7 +52,6 @@ public class BookModel {
         Novate mNovate = new Novate.Builder(mContext).build();
         Map<String, Object> mStringObjectMap = new HashMap<>();
         mStringObjectMap.put("cpage", mCurrpage);
-        mStringObjectMap.put("pagesize", 2);
         mNovate.executeGet(ApiInterface.COLLECT_BOOKDISPLAY, mStringObjectMap, mBack, SPUtils.getInstance().getToken(mContext));
     }
 
